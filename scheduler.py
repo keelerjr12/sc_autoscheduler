5b17d588b6cce419a956ef8a336b89d41b334ae6
Josh Keeler <keelerjr12@gmail.com>
2022-11-09 17:05:42 -0600

    adding unit tests

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,82 @@
+from abc import ABC, abstractmethod
+from datetime import datetime, timedelta
+from enum import Flag, auto
 from ortools.sat.python import cp_model
+
+class DutyType(Flag):
+    CONTROLLER = auto()
+    OBSERVER = auto()
+    RECORDER = auto()
+    SPOTTER = auto()
+    LONER = auto()
+    OPS_SUP = auto()
+    SOF = auto()
+
+def str_to_duty_type(str_type: str) -> DutyType:
+    lc_str_type = str_type.lower()
+
+    if lc_str_type.find('controller') != -1:
+        return DutyType.CONTROLLER
+    elif lc_str_type.find('observer') != -1:
+        return DutyType.OBSERVER
+    elif lc_str_type.find('recorder') != -1:
+        return DutyType.RECORDER
+    elif lc_str_type.find('spotter') != -1:
+        return DutyType.SPOTTER
+    elif lc_str_type.find('loner') != -1:
+        return DutyType.LONER
+    elif lc_str_type.find('sof') != -1:
+        return DutyType.SOF
+    else:
+        return DutyType.OPS_SUP
+
+class Person:
+    prsn_id: int
+    _first_name: str
+    _last_name: str
+    _quals: DutyType
+
+    def __init__(self, prsn_id: int, last_name: str, first_name: str):
+        self.prsn_id = prsn_id
+        self._first_name = first_name
+        self._last_name = last_name
+        self._quals: DutyType = DutyType.CONTROLLER & DutyType.OBSERVER
+
+    def qual(self, type: DutyType):
+        self._quals = self._quals | type
+
+    def is_qualified(self, type: DutyType) -> bool:
+        return (self._quals & type) != (DutyType.CONTROLLER & ~DutyType.CONTROLLER) ## TODO: This is a very hacky way for bitwise 0
+
+class Commitment(ABC):
+    def is_conflict(self, other) -> bool:
+        return other.end_dt() > self.start_dt() and other.start_dt() < self.end_dt()
+
+    @abstractmethod
+    def start_dt(self) -> datetime:
+        pass
+
+    @abstractmethod
+    def end_dt(self) -> datetime:
+        pass
+
+class Duty(Commitment):
+    name: str
+    type: DutyType
+    _sign_in_dt: datetime
+    _sign_out_dt: datetime
+
+    def __init__(self, name: str, type: DutyType, sign_in_dt: datetime, sign_out_dt: datetime):
+        self.name = name
+        self.type = type
+        self._sign_in_dt = sign_in_dt
+        self._sign_out_dt = sign_out_dt
+
+    def start_dt(self) -> datetime:
+        return self._sign_in_dt
+
+    def end_dt(self) -> datetime:
+        return self._sign_out_dt
 
 class ScheduleModel:
     duties = []
@@ -19,14 +97,34 @@ class ScheduleSolver:
         model = cp_model.CpModel()
 
         self._add_variables(model)
+        self._add_constraints(model)
 
         solver = cp_model.CpSolver()
+        status = solver.Solve(model)
 
-        return solver.Solve(model)
+        if status != cp_model.OPTIMAL:
+            raise Exception("Suboptimal solution")
+            
+        return self._get_solution(solver)
+
 
     def _add_variables(self, model: cp_model.CpModel):
-
         # create all duty variables
         for duty in self._sched_model.duties:
             for person in self._sched_model.personnel:
-                _duty_schedule_vars[(duty.name, person.prsn_id)] = model.NewBoolVar('duty_s%s%i' % (duty.name, person.prsn_id))    
+                self._duty_schedule_vars[(duty.name, person.prsn_id)] = model.NewBoolVar('duty_s%s%i' % (duty.name, person.prsn_id))    
+
+    def _add_constraints(self, model: cp_model.CpModel):
+        # all duties filled with respective qual'd personnel
+        for duty in self._sched_model.duties:
+            model.AddExactlyOne(self._duty_schedule_vars[(duty.name, p.prsn_id)] for p in self._sched_model.personnel)
+
+    def _get_solution(self, solver: cp_model.CpSolver):
+        solution = {}
+
+        for d in self._sched_model.duties:
+            for p in self._sched_model.personnel:
+                if solver.Value(self._duty_schedule_vars[(d.name, p.prsn_id)]):
+                    solution[d.name] = p.prsn_id
+
+        return solution
--- test_solver.py
+++ test_solver.py
@@ -1,14 +1,15 @@
 import pytest
+from datetime import datetime, timedelta
 from ortools.sat.python import cp_model
-from scheduler import ScheduleModel, ScheduleSolver
+from scheduler import ScheduleModel, ScheduleSolver, Person, Duty, DutyType
 
-def test_lines_filled():
-    duties = []
-    personnel = []
+def test_given_single_duty_and_single_person_when_solved_then_duty_is_filled():
+    duties = [Duty("Tinder 1 Controller", DutyType.CONTROLLER, datetime.strptime('7/29/2022 8:00:00 AM', '%m/%d/%Y %I:%M:%S %p'), datetime.strptime('7/29/2022 10:00:00 AM', '%m/%d/%Y %I:%M:%S %p'))]
+    personnel = [Person(1, "LastName", "FirstName")]
 
     model = ScheduleModel(duties, personnel)
     solver = ScheduleSolver(model)
 
-    status = solver.solve()
+    solution = solver.solve()
 
-    assert status == cp_model.OPTIMAL
+    assert solution == {duties[0].name: personnel[0].prsn_id}
