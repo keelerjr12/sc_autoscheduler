dd9484755306fd984775cb618c2877ee9b013cad
Josh Keeler <keelerjr12@gmail.com>
2022-12-12 21:58:27 -0600

    added db functionality

---
+++ web/schedule/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import HttpResponse, render
+import csv
+from .models import Pilot
+
+def index(request):
+    pilots_rs = Pilot.objects.all()
+    header_quals = ['Operations Supervisor', 'SOF', 'RSU Controller', 'RSU Observer']
+
+    pilots = []
+    for pilot_rs in pilots_rs:
+        pilot = {'last_name': pilot_rs.last_name, 'first_name': pilot_rs.first_name}
+        quals = []
+        for qual in header_quals:
+            print(qual)
+            q = ' '
+            for pq in pilot_rs.quals.all():
+                if pq.name == qual:
+                    q = 'X'
+            quals.append(q)
+        pilot['quals'] = quals
+        pilots.append(pilot)
+
+    context = {'header': header_quals, 'pilots': pilots}
+    #file = 'res/lox.csv'
+
+    #with open(file, newline='') as csvfile:
+    #    reader = csv.reader(csvfile, delimiter=',')
+    #    header = next(reader, None)
+
+    #    roster = [row for row in reader]
+    #    context = {'header': header, 'roster': roster}
+
+    return render(request, 'schedule/index.html', context)
